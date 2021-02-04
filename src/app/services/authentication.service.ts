1fd78772ef47d397b0bec043c7b26084d1558849
Frédéric LAUDARIN <Frederic.LAUDARIN@modiscloud.net>
2021-02-04 15:16:11 +0100

    feat(component): header with nav-bar

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
 import { AuthStorageModel } from '../models/auth-storage-model';
 import { SignInCredentialsModel } from '../models/sign-in-credentials-model';
 import { WebStorageService } from './web-storage.service';
 
-const users = new Map<string, string>([['TESTING', 'Testing2020']]);
+// This data should be on the backend server
+const users = new Map<string, string>([['TESTING', 'TestingSandBox21']]);
+/* Token's payload
+  {
+    "sub": "1234567890",
+    "name": "Testing",
+    "iat": 1612432959, (feb-04-2021)
+    "iss": "The fake backend",
+    "exp": 1770220959  (+5 years)
+  }
+ */
+const fakeToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlRlc3RpbmciLCJpYXQiOjE2MTI0MzI5NTksImlzcyI6IlRoZSBmYWtlIGJhY2tlbmQiLCJleHAiOjE3NzAyMjA5NTl9.xsKcgc3AwCXRETUBUdV7VM6bC7VRPJB_71-8WoUixNc';
 
 @Injectable({
   providedIn: 'root',
@@ -16,14 +28,7 @@ export class AuthenticationService {
     return new Observable((subscriber) => {
       // Authentication data are stored
       if (this.storageService.getItem('app-AUTH')) {
-        const authData = <AuthStorageModel>(
-          JSON.parse(this.storageService.getItem('app-AUTH'))
-        );
-        if (this.checkToken(authData.token)) {
-          subscriber.next(true);
-        } else {
-          subscriber.next(false);
-        }
+        this.checkTokenInStorage(subscriber);
       }
 
       // No credentials provided
@@ -32,18 +37,7 @@ export class AuthenticationService {
       }
       // The user is registered
       else if (users.has(credentials.login)) {
-        // The passwords match
-        if (users.get(credentials.login) === credentials.password) {
-          // Dummy token
-          const authData: AuthStorageModel = {
-            login: credentials.login,
-            token: '123456789ABCDEF',
-          };
-
-          this.storageService.clearStorage();
-          this.storageService.setItem('app-AUTH', JSON.stringify(authData));
-          subscriber.next(true);
-        }
+        this.validateCredentials(credentials, subscriber);
       } else {
         // The user is not registered or the password is wrong
         subscriber.next(false);
@@ -51,7 +45,44 @@ export class AuthenticationService {
     });
   }
 
+  private validateCredentials(
+    credentials: SignInCredentialsModel,
+    subscriber: Subscriber<boolean>
+  ) {
+    // The passwords match
+    if (users.get(credentials.login) === credentials.password) {
+      this.getToken(credentials.login).then((token) => {
+        // Dummy token
+        const authData: AuthStorageModel = {
+          login: credentials.login,
+          token,
+        };
+
+        this.storageService.clearStorage();
+        this.storageService.setItem('app-AUTH', JSON.stringify(authData));
+        subscriber.next(true);
+      });
+    }
+  }
+
+  private checkTokenInStorage(subscriber: Subscriber<boolean>) {
+    const authData = <AuthStorageModel>(
+      JSON.parse(this.storageService.getItem('app-AUTH'))
+    );
+    if (this.checkToken(authData.token)) {
+      subscriber.next(true);
+    } else {
+      subscriber.next(false);
+    }
+  }
+
   private checkToken(token: string): boolean {
     return token ? true : false;
+  }
+
+  private getToken(login: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      resolve(fakeToken);
+    });
   }
 }

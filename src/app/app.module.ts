f3db74e38327db41600a3e7b8f16eeb522db598f
Frédéric LAUDARIN <Frederic.LAUDARIN@modiscloud.net>
2021-02-04 22:24:09 +0100

    chore: integration of Plotly.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,16 @@ import { FormsModule } from '@angular/forms';
 import { MainComponent } from './components/main/main.component';
 import { HeaderComponent } from './components/main/header/header.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { ChartDemoComponent } from './components/chart-demo/chart-demo.component';
 
 @NgModule({
-  declarations: [AppComponent, SignInComponent, MainComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    SignInComponent,
+    MainComponent,
+    HeaderComponent,
+    ChartDemoComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/components/chart-demo/chart-demo.component.ts
@@ -0,0 +1,32 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+// this is the global hook to the bokehjs lib (without types)
+// import { Data, plot } from 'plotly.js';
+declare var Plotly: any;
+
+@Component({
+  selector: 'app-chart-demo',
+  templateUrl: './chart-demo.component.html',
+  styleUrls: ['./chart-demo.component.css'],
+})
+export class ChartDemoComponent implements OnInit {
+  @ViewChild('Graph', { static: true })
+  private Graph: ElementRef;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log(this.Graph);
+    Plotly.newPlot(
+      this.Graph.nativeElement,
+      [
+        {
+          x: [1, 2, 3, 4, 5],
+          y: [1, 2, 4, 8, 16],
+        },
+      ],
+      {
+        margin: { t: 0 },
+      }
+    );
+  }
+}
